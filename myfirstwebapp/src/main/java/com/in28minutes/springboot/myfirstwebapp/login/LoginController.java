263ac6e6a3277a64b5e5efe52c9b6014caa59e64
Sertan Ortaoğlan <sertanortaoglan@hotmail.com>
2024-12-30 00:04:50 +0300

    @SessionAttributes added

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")       //Değerleri session içerisine koymanın yolu '@SessionAttributes' annotation'ı kullanmaktır. Hangi özelliği(attribute) session'a koymak istiyorsak(örneğin: "name") onu parantez içerisine yazarız. Daha sonra bu session içerisine koyduğumuz "name" attribute'unu buradan başka hangi controller'da kullanmak istiyorsak(mesela 'TodoController') o controller içerisine de aynı burada yazdığımız şekilde yazarak[@SessionAttributes("name") şeklinde] kullanımı gerçekleştirebiliriz. 
 public class LoginController {
 	
 	//Authentication işlemi yapacağız. "name:in28minutes password: dummy" girilirse giriş sayfasına girmesine izin vereceğiz. Aksi taktirde login sayfasına geri yönlendireceğiz. Bunun işlem için "AuthenticationService" adında yeni bir class oluşturup gerekli methodları yazıp buraya enjekte ederiz.
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -5,5 +5,7 @@
 	<body>
 		<div>Welcome to in28minutes</div>
 		<div>Your name: ${name}</div>
+		<div><a href="list-todos">Manage</a> your todos</div>
 	</body>
 </html>
+ 
